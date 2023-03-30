3c665ce9a42435be35a7d081ea7bb8a8381ab9d3
Кирилл Острогов <kirill.ost94@gmail.com>
2023-03-30 11:46:23 +0300

    Коммит перед ревью

---
+++ yatube/posts/views.py
@@ -0,0 +1,92 @@
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import PostForm
+from .models import Post, Group, User
+
+SELECT_LIMIT = 10
+
+
+def index(request):
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, SELECT_LIMIT)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()
+    paginator = Paginator(posts, SELECT_LIMIT)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'group': group,
+        'page_obj': page_obj
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    # Здесь код запроса к модели и создание словаря контекста
+    author = get_object_or_404(User, username=username)
+    posts_author = author.posts.all()
+    paginator = Paginator(posts_author, SELECT_LIMIT)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'page_obj': page_obj
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    # Здесь код запроса к модели и создание словаря контекста
+    posts = get_object_or_404(Post, pk=post_id)
+    count = Post.objects.filter(author_id=posts.author_id).count()
+    context = {
+        'post': posts,
+        'post_count': count
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    form = PostForm(
+        request.POST or None,
+        files=request.FILES or None
+    )
+    if not form.is_valid():
+        return render(request, 'posts/create_post.html', {'form': form})
+    post = form.save(commit=False)
+    post.author = request.user
+    post.save()
+    return redirect('posts:profile', username=post.author)
+
+
+@login_required
+def post_edit(request, post_id):
+    """Функция для редактирования записи."""
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect('posts:post_detail', post_id=post_id)
+    form = PostForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=post,
+    )
+    if not form.is_valid():
+        return render(request, 'posts/create_post.html', {
+            'post': post,
+            'form': form,
+            'is_edit': True
+        })
+    form.save()
+    return redirect('posts:post_detail', post_id=post_id)
---
+++ yatube/users/views.py
@@ -0,0 +1,21 @@
+# Импортируем CreateView, чтобы создать ему наследника
+from django.views.generic import CreateView
+
+# Функция reverse_lazy позволяет получить URL по параметрам функции path()
+# Берём, тоже пригодится
+from django.urls import reverse_lazy
+
+# Импортируем класс формы, чтобы сослаться на неё во view-классе
+from .forms import CreationForm
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
+
+
+class PasswordResetForm(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/password_reset_form.html'
